e1d8bf4b09a7e16ca3754cff1d7cf9f4e98a5d26
Fredrik Bore <fredrik.bore@geodata.no>
2019-12-02 00:29:17 +0100

    Improve config handling

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.8-slim
 
+ENV DATA_FOLDER="/config"
+
 WORKDIR /app
-COPY /requirements.txt /trackerstats.yaml /trackerstats.py /app/
+COPY /requirements.txt /trackerstats.example.yaml /trackerstats.py /app/
 COPY /trackerstats /app/trackerstats
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
-CMD python /app/trackerstats.py
+CMD cp /app/trackerstats.example.yaml /config/trackerstats.example.yaml && python /app/trackerstats.py
 
 VOLUME /config
--- trackerstats.py
+++ trackerstats.py
@@ -1,12 +1,22 @@
 import time
 import yaml
+from os import environ as env
+from os.path import abspath, dirname, join, exists
+from shutil import copyfile
 
 from trackerstats.dbmanager import DBManager
 from trackerstats.trackers.torrentday import TorrentDay
 
 
 if __name__ == "__main__":
-    with open('./trackerstats.yaml') as f:
+    DATA_FOLDER = env.get('DATA_FOLDER', abspath(dirname(__file__)))
+
+    config_file_path = join(DATA_FOLDER, 'trackerstats.yaml')
+
+    if not exists(config_file_path):
+        copyfile(join(DATA_FOLDER, 'trackerstats.example.yaml'), config_file_path)
+
+    with open(join(DATA_FOLDER, 'trackerstats.yaml')) as f:
         CONFIG = yaml.safe_load(f)
 
     DBMANAGER = DBManager(CONFIG['influxdb'])
