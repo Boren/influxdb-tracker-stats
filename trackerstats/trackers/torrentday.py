e95372e8f0d85afaf1197e2486192316581943d0
Fredrik Bore <fredrik.bore@geodata.no>
2019-12-01 21:36:07 +0100

    TorrentDay: Add ratio and invites

--- trackerstats/trackers/torrentday.py
+++ trackerstats/trackers/torrentday.py
@@ -10,7 +10,7 @@ class TorrentDay(object):
     def __init__(self, dbmanager, config):
         self.dbmanager = dbmanager
         self.name = "TorrentDay"
-        self.base_url = "https://www.torrentday.com/"
+        self.base_url = "https://www.torrentday.com"
         self.logger = getLogger()
         self.cookies = SimpleCookie()
         self.cookies.load(config['cookies'])
@@ -25,33 +25,51 @@ class TorrentDay(object):
                 cookies[key] = morsel.value
             response = s.get(self.base_url, cookies=cookies)
 
-        soup = BeautifulSoup(response.content, "html.parser")
-        activity = soup.find(id="activityDiv").find_all("span")
+            soup = BeautifulSoup(response.content, "html.parser")
+            activity = soup.find(id="activityDiv").find_all("span")
 
-        ratio = float(activity[1].string)
-        upload = humanfriendly.parse_size(activity[3].string)
-        download = humanfriendly.parse_size(activity[5].string)
+            ratio = float(activity[1].string)
+            torrents_downloading = int(activity[6].string)
+            torrents_uploading = int(activity[7].string)
 
-        torrents_downloading = int(activity[6].string)
-        torrents_uploading = int(activity[7].string)
+            bonus_points_string = soup.select_one("a[href*=mybonus]").string
+            bonus_points = bonus_points_string[bonus_points_string.find("(")+1:bonus_points_string.find(")")]
+            bonus_points = int(float(bonus_points))
 
-        influx_payload.append(
-            {
-                "measurement": "stats",
-                "tags": {
-                    "tracker": self.name,
+            invites_string = soup.select_one("a[href*=invite]").string
+            invites = invites_string[invites_string.find("(")+1:invites_string.find(")")]
+            invites = int(invites)
+
+            # One more decimal for data in user profile
+            userprofilelink = soup.find(id="dropDownHdr").find("a")
+
+            userresponse = s.get(self.base_url + userprofilelink['href'], cookies=cookies)
+            usersoup = BeautifulSoup(userresponse.content, "html.parser")
+            userdetails = usersoup.find(id="usrDetailsInfoBarSpans").find_all("span", class_="detailsInfoSpan")
+
+            upload = humanfriendly.parse_size(userdetails[3].find("span").string)
+            download = humanfriendly.parse_size(userdetails[4].find("span").string)
+
+            influx_payload.append(
+                {
+                    "measurement": "stats",
+                    "tags": {
+                        "tracker": self.name,
+                    },
+                    "time": now,
+                    "fields": {
+                        "ratio": ratio,
+                        "download": download,
+                        "upload": upload,
+                        "torrents_downloading": torrents_downloading,
+                        "torrents_uploading": torrents_uploading,
+                        "bonus_points": bonus_points,
+                        "invites": invites
+                    },
                 },
-                "time": now,
-                "fields": {
-                    "ratio": ratio,
-                    "download": download,
-                    "upload": upload,
-                    "torrents_downloading": torrents_downloading,
-                    "torrents_uploading": torrents_uploading,
-                },
-            },
-        )
+            )
 
-        self.logger.info("Updated TorrentDay")
+            self.logger.info("Updated TorrentDay")
+            print("Updated torrentday")
 
-        self.dbmanager.write_points(influx_payload)
+            self.dbmanager.write_points(influx_payload)
